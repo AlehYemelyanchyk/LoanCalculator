024d1a2d84f5ff27299014e2397ba132e1d4f766
Aleh <yem1403@gmail.com>
2019-05-14 21:13:28 +0300

    line down

--- src/com/alehyemelyanchyk/Main.java
+++ src/com/alehyemelyanchyk/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
 
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, in order to get a loan, fill the information:");
         System.out.println("The amount of loan you would like to get:");
